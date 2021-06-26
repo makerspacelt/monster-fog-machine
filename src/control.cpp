dd7c9fad17f617d68e91f9f816b84c46cd034ffc
Martynas Jonaitis <marjon3@ktu.lt>
2021-06-26 21:30:50 +0300

    Implemented manual control mode and activation counter

--- src/control.cpp
+++ src/control.cpp
@@ -6,10 +6,15 @@ const int HEATER_3_VOLTAGE_IND_PIN = 5;
 const int FLUID_TANK_FLOAT_PIN = 5;
 const int MACHINE_CONTROL_RELAY_PIN = 2;
 
-bool timerControlActive = false;
+const int CONTROL_MODE_NONE = 0;
+const int CONTROL_MODE_MANUAL = 0;
+const int CONTROL_MODE_AUTO = 0;
+
+int controlMode = CONTROL_MODE_NONE;
 
 float activationFrequency = 5 * 1000;
 float activationLength = 10 * 1000; 
+int activationCountSetting = 10;
 
 long controlTimer;
 
@@ -20,6 +25,7 @@ bool fluidTankState = false;
 
 bool machineReady = false;
 int controlState = -1; // 0 -> machine is stopped, waiting for next activation (waiting stage), 1 -> machine is started (activation stage)
+int activationCounter = 0;
 
 void setup_control_module() {
     pinMode(MACHINE_CONTROL_RELAY_PIN, OUTPUT);
@@ -27,8 +33,6 @@ void setup_control_module() {
     pinMode(HEATER_2_VOLTAGE_IND_PIN, INPUT);
     pinMode(HEATER_3_VOLTAGE_IND_PIN, INPUT);
     pinMode(FLUID_TANK_FLOAT_PIN, INPUT);
-
-    timerControlActive = true; //REMOVE AFTER DEBUG
 }
 
 void updateFogMachineState() {
@@ -56,45 +60,60 @@ void control_loop() {
     updateFogMachineState();
     
     // Checking if timer control is activated and all machine requirements are met
-    if(!timerControlActive || !machineReady) {
+    if(!machineReady || controlMode == CONTROL_MODE_NONE) {
         // Stops the automatic and manual control of the machine and resets control variables to undefined
         
         stop_machine();
         controlState = -1;
         controlTimer = -1;
+        activationCounter = 0;
+        controlMode = CONTROL_MODE_NONE;
+
         return;
     }
 
-    if(controlTimer == -1 || controlState == -1) {
-        // Initial automatic control variables
-        controlTimer = millis();
-        controlState = 1;
-    }
-
-    // Machine activation stage
-    if (controlState == 1) {
-        if (millis() - controlTimer < activationLength) {
-            // Activates machine
-            start_machine();
-        } else {
-            // Initiates waiting stage
+    if (controlMode == CONTROL_MODE_AUTO) {
+        if(controlTimer == -1 || controlState == -1) {
+            // Initial automatic control variables
+            controlTimer = millis();
             controlState = 0;
-            controlTimer = millis();
-            stop_machine();
         }
-    }
 
-    // Waiting for next activation stage
-    if (controlState == 0) {
-        if (millis() - controlTimer < activationFrequency) {
-            // Stops machine
-            stop_machine();
-        } else {
-            // Initiates activation stage
-            controlState = 1;
-            controlTimer = millis();
-            start_machine();
+        // Machine activation stage
+        if (controlState == 1) {
+            if (millis() - controlTimer < activationLength) {
+                // Activates machine
+                start_machine();
+            } else {
+                // Initiates waiting stage and adds to the counter
+                controlState = 0;
+                controlTimer = millis();
+                stop_machine();
+                activationCounter = activationCounter + 1;
+            }
         }
-    }
 
+        // Waiting for next activation stage
+        if (controlState == 0) {
+            if (millis() - controlTimer < activationFrequency) {
+                // Stops machine
+                stop_machine();
+            } else {
+                // Initiates activation stage
+                controlState = 1;
+                controlTimer = millis();
+                start_machine();
+            }
+        }
+
+        if (activationCounter > activationCountSetting) {
+            // Finishes the automatic control
+            // Variables are resetted in the next control loop
+            controlMode = CONTROL_MODE_NONE;
+        }
+    } if (controlMode == CONTROL_MODE_AUTO) {
+        // Activates machine
+        // State validation is already performed at this point
+        start_machine();
+    }
 }
