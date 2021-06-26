8ecb14852fa8107af0cffe904864b5a944779ca2
unknown <you@example.com>
2021-06-26 14:29:16 +0300

    simplified heater response

--- data/espemu.php
+++ data/espemu.php
@@ -40,10 +40,10 @@ function do_stopManual() {
 
 function do_startAuto() {
     // response: true if started, false if not
-    $sensor = explode('?', $_GET['f']);
-    if (count($sensor) > 1) {
-        $val = explode('=', trim($sensor[1]));
-        if ((count($val) > 1) && ($val[1] != '')) {
+    $params = explode('?', $_GET['f']);
+    if (count($params) > 1) {
+        $sensorVal = explode('=', trim($params[1]));
+        if ((count($sensorVal) > 1) && ($sensorVal[1] != '')) {
             echo (rand(0, 1) == 0 ? 'false' : 'true');
         } else {
             echo 'no sensor value';
@@ -59,17 +59,7 @@ function do_stopAuto() {
 
 function do_heaterStatus() {
     // response: true if ready, false if heating
-    $params = explode('?', $_GET['f']);
-    if (count($params) > 1) {
-        $sensorVal = explode('=', trim($params[1]));
-        if ((count($sensorVal) > 1) && ($sensorVal[1] != '')) {
-            echo (rand(0, 1) == 0 ? 'false' : 'true');
-        } else {
-            echo 'no sensor value';
-        }
-    } else {
-        echo 'no sensor param';
-    }
+    echo (rand(0, 1) == 0 ? 'false' : 'true');
 }
 
 function do_liquidStatus() {
