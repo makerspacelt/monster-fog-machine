9485c1c481278e56f87baf73a4f5d859670cac66
Kulverstukas <kulverstukas.k@gmail.com>
2022-06-19 21:44:03 +0300

    added some code to begin with

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <max6675.h>
+
+#define SSR_PIN D3
+#define BTN_PIN D7
+#define THERMO_DO D6
+#define THERMO_CS D8
+#define THERMO_CLK D5
+
+MAX6675 thermo(THERMO_CLK, THERMO_CS, THERMO_DO);
 
 void setup() {
     Serial.begin(115200);
+
+    pinMode(SSR_PIN, OUTPUT);
+    pinMode(BTN_PIN, INPUT_PULLUP);
+    
     LiquidCrystal_I2C lcd(0x27, 16, 2);
     lcd.init();
     lcd.clear();
