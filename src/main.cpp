4249659b3f30ce721d6fff91c6d11c57fd8a591d
Kulverstukas <kulverstukas.k@gmail.com>
2022-07-04 20:09:50 +0300

    removed EEPROM usage, button action and changed some variable names

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,18 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <max6675.h>
-#include <EEPROM.h>
 
 #define SSR_PIN D3 // Solid State Relay
-#define PUMP_BTN_PIN D7
 #define ERR_LED_PIN D0
-#define THERMO_DO D6
+#define THERMO_SO D6
 #define THERMO_CS D8
-#define THERMO_CLK D5
+#define THERMO_SCK D5
 #define HEATER_CRITICAL_TEMP 150.0 // when to halt everything
 #define HEATER_STOP_TEMP 100.0 // when to shut off the heaters
 #define HEATER_START_TEMP 60.0 // below this number we start the heaters
 #define HEATER_POLL_TIME 1000 // how often to check block temp
 
-MAX6675 thermo(THERMO_CLK, THERMO_CS, THERMO_DO);
+MAX6675 thermo(THERMO_SCK, THERMO_CS, THERMO_SO);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 bool halt;
 unsigned long lastMillis;
@@ -37,33 +35,14 @@ void setup() {
     Serial.begin(115200);
     Serial.println();
 
-    EEPROM.begin(1);
-
     pinMode(SSR_PIN, OUTPUT);
     pinMode(ERR_LED_PIN, OUTPUT);
-    pinMode(PUMP_BTN_PIN, INPUT_PULLUP);
 
     digitalWrite(SSR_PIN, LOW);
 
     lcd.init();
     lcd.clear();
     lcd.backlight();
-
-    // reset self-destruction status
-    if (digitalRead(PUMP_BTN_PIN) == LOW) {
-        EEPROM.write(0, 0);
-        EEPROM.commit();
-    } else {
-        // has the block attempted self-destruction before?
-        int val = EEPROM.read(0);
-        // Serial.print(val);
-        if (val != 0) {
-            lcd.setCursor(3, 0);
-            lcd.print("OVER-HEAT");
-            halt = true;
-            return;
-        }
-    }
 
     // check initial temperature before anything else
     // shut down if the block is nearing self-destruction
@@ -77,19 +56,12 @@ void setup() {
     } else if (blockTemp >= HEATER_CRITICAL_TEMP) {
         lcd.setCursor(3, 0);
         lcd.print("OVER-HEAT");
-        EEPROM.write(0, 1);
-        EEPROM.commit();
         halt = true;
         return;
     }
 }
 
 void loop() {
-    // always allow to spray in case we need to cool it down quickly
-    if (digitalRead(PUMP_BTN_PIN) == LOW) {
-        
-    }
-
     if (halt) {
         if (digitalRead(SSR_PIN) != LOW) digitalWrite(SSR_PIN, LOW);
         if ((millis() - lastMillis) >= 1000) {
@@ -103,14 +75,12 @@ void loop() {
         lastMillis = millis();
         float blockTemp = thermo.readCelsius();
         if (!isnan(blockTemp)) {
-            Serial.print(blockTemp);
+            // Serial.print(blockTemp);
             updateTempOnScreen(blockTemp);
             if (blockTemp >= HEATER_CRITICAL_TEMP) {
                 lcd.clear();
                 lcd.setCursor(3, 0);
                 lcd.print("OVER-HEAT");
-                EEPROM.write(0, 1);
-                EEPROM.commit();
                 halt = true;
                 return;
             } else if (blockTemp >= HEATER_STOP_TEMP) {
