9d5116e1a68d5a41fab034e2414a411b2f5457b1
Kulverstukas <kulverstukas.k@gmail.com>
2022-07-27 13:01:43 +0300

    changed temperature to normal; added status led; done some minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,15 @@
 
 #define SSR_PIN D3 // Solid State Relay
 #define ERR_LED_PIN D0
+#define READY_LED_PIN 3 // RX pin
 #define TEMP_PIN D7 // DS18B20 sensor
 #define THERMO_SO D6 // for Thermocoupler reading
 #define THERMO_CS D8 // for Thermocoupler reading
 #define THERMO_SCK D5 // for Thermocoupler reading
-#define HEATER_CRITICAL_TEMP 150.0 // when to halt everything
-#define HEATER_STOP_TEMP 100.0 // when to shut off the heaters
-#define HEATER_START_TEMP 80.0 // below this number we start the heaters
+#define HEATER_CRITICAL_TEMP 400.0 // when to halt everything
+#define HEATER_STOP_TEMP 300.0 // when to shut off the heaters
+#define HEATER_START_TEMP 290.0 // below this number we start the heaters
+#define HEATER_NOT_READY_TEMP 250.0 // below this number we notify the user to stop
 #define HEATER_POLL_TIME 1000 // how often to check block temp
 
 MAX6675 thermo(THERMO_SCK, THERMO_CS, THERMO_SO);
@@ -47,8 +49,10 @@ void setup() {
 
     pinMode(SSR_PIN, OUTPUT);
     pinMode(ERR_LED_PIN, OUTPUT);
+    pinMode(READY_LED_PIN, OUTPUT);
 
     digitalWrite(SSR_PIN, LOW);
+    digitalWrite(READY_LED_PIN, HIGH);
     dtSensor.begin();
 
     lcd.init();
@@ -64,19 +68,32 @@ void setup() {
         // thermocoupler not attached!
         lcd.setCursor(2, 1);
         lcd.print("CHECK THRMCP");
+        Serial.println("CHECK THRMCP");
         halt = true;
         return;
     } else if (caseTemp == DEVICE_DISCONNECTED_C) {
         // case temp sensor not attached!
         lcd.setCursor(2, 1);
         lcd.print("CHECK SENSOR");
+        Serial.println("CHECK SENSOR");
         halt = true;
         return;
     } else if (blockTemp >= HEATER_CRITICAL_TEMP) {
         lcd.setCursor(3, 1);
         lcd.print("OVER-HEAT");
+        Serial.println("OVER-HEAT");
         halt = true;
         return;
+    } else if (blockTemp >= HEATER_START_TEMP) {
+        lcd.setCursor(4, 1);
+        lcd.print("Ready  ");
+        Serial.println("Ready  ");
+    }
+
+    if (!halt && (blockTemp < HEATER_NOT_READY_TEMP)) {
+        digitalWrite(READY_LED_PIN, HIGH);
+    } else if (!halt && (blockTemp >= HEATER_STOP_TEMP)) {
+        digitalWrite(READY_LED_PIN, LOW);
     }
 }
 
@@ -94,12 +111,14 @@ void loop() {
         float blockTemp = thermo.readCelsius();
         dtSensor.requestTemperatures();
         float caseTemp = dtSensor.getTempCByIndex(0);
-        Serial.println(caseTemp);
+        // Serial.println(caseTemp);
 
         if (caseTemp == DEVICE_DISCONNECTED_C) {
             // case temp sensor not attached!
             lcd.setCursor(2, 1);
             lcd.print("CHECK SENSOR");
+            digitalWrite(READY_LED_PIN, HIGH);
+            Serial.println("CHECK SENSOR");
             halt = true;
         }
 
@@ -109,23 +128,33 @@ void loop() {
             if (!halt && (blockTemp >= HEATER_CRITICAL_TEMP)) {
                 lcd.setCursor(3, 1);
                 lcd.print("OVER-HEAT");
+                digitalWrite(READY_LED_PIN, HIGH);
+                Serial.println("OVER-HEAT");
                 halt = true;
                 return;
-            } else if (!halt && ((blockTemp >= HEATER_STOP_TEMP) || (blockTemp >= HEATER_START_TEMP))) {
+            } else if (!halt && (blockTemp >= HEATER_STOP_TEMP)) {
                 digitalWrite(SSR_PIN, LOW);
-                lcd.setCursor(4, 1);
+                digitalWrite(READY_LED_PIN, LOW);
                 lcd.setCursor(4, 1);
                 lcd.print("Ready  ");
+                Serial.println("Ready  ");
             } else if (!halt && (blockTemp <= HEATER_START_TEMP)) {
                 digitalWrite(SSR_PIN, HIGH);
                 lcd.setCursor(4, 1);
                 lcd.print("Heating");
+                Serial.println("Heating");
+            }
+
+            if (!halt && (blockTemp < HEATER_NOT_READY_TEMP)) {
+                digitalWrite(READY_LED_PIN, HIGH);
             }
         } else {
             // thermocoupler not attached!
             lcd.clear();
             lcd.setCursor(2, 1);
             lcd.print("CHECK THRMCP");
+            digitalWrite(READY_LED_PIN, HIGH);
+            Serial.println("CHECK THRMCP");
             halt = true;
             return;
         }
